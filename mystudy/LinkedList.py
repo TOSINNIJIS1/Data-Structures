962aa7dab0380a698a2f055c157aded6a0c8b11e
VINCENT ADENIJI <54925113+TOSINNIJIS1@users.noreply.github.com>
2020-09-24 23:00:23 -0400

    done yay

--- mystudy/LinkedList.py
+++ mystudy/LinkedList.py
@@ -20,3 +20,42 @@ ll.next.next = Node(3)
 # We need to start from the head to the tail 
 # Head => We had to start from the head and traverse all the way to the end of the linked list to add a new element
 # Tail
+
+# Binary Search
+    # def iteration_depth_first_for_each(self, fn):
+    #     # DFO: LIFO
+    #     # Stack
+    #     stack = []
+    #     stack.append(self)
+
+    #     while len(stack) > 0:
+    #         current = stack.pop()
+
+    #         if current.right:
+    #             stack.append(current.right)
+            
+    #         if current.left:
+    #             stack.append(current.left)
+
+    #         fn(current.value)
+
+    
+    # def iteration_breadth_first_for_each(self, fn):
+    #     from collections import deque
+
+    #     # BFT: FIFO
+    #     # Queue
+
+    #     queue = deque()
+    #     queue.append(self)
+
+    #     while len(queue) > 0:
+    #         current = queue.popleft()
+
+    #         if current.left:
+    #             queue.append(current.left)
+
+    #         if current.right:
+    #             queue.append(current.right)
+
+    #         fn(current.value)
